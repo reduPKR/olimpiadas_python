de22b53b899240bd1c301274928122987eac4804
redupkr <reduardo1994@hotmail.com>
2020-10-24 00:53:01 -0300

    Models criadas

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Sport)
+admin.site.register(Sex)
+admin.site.register(Athlete)
+admin.site.register(Season)
+admin.site.register(Game)
+admin.site.register(AthleteGame)
+admin.site.register(Event)
+admin.site.register(Medal)
+admin.site.register(GameEvents)
