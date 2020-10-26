f6355a8c9803c7ad3ba35bc8319377ef9da486e1
redupkr <reduardo1994@hotmail.com>
2020-10-26 13:38:48 -0300

    merge com 04

--- core/controllers/upload/views.py
+++ core/controllers/upload/views.py
@@ -41,6 +41,8 @@ def save_data(athletes, regions, request):
     save_season(athletes["Season"], request)
     save_game(athletes[["Year", "Season", "City"]], request)
     save_game_event(athletes[["Year", "Season", "City", "Event"]], request)
+    save_athlete(athletes[["ID", "Name", "Sex", "Height", "Weight", "NOC", "Sport"]], request)
+    save_event_participants(athletes[["Name", "Sex", "Height", "Weight",  "NOC", "Sport", "Age", "Year", "Season", "City", "Event", "Medal"]], request)
 
 def save_region(regions, request):
     regions.fillna(value="", inplace=True)
@@ -55,6 +57,9 @@ def save_region(regions, request):
 
 def region_not_exist(noc):
     return Country.objects.filter(noc=noc).first() == None
+
+def get_region_by_noc(noc):
+    return Country.objects.get(noc=noc)
 
 def register_country(country):
     Country.objects.create(
@@ -197,7 +202,7 @@ def save_game_event(game_event, request):
             if not game_not_exist(item[1]["Year"], item[1]["Season"], item[1]["City"]):
                 if game_event_not_exist(item[1]["Year"], item[1]["Season"], item[1]["City"], item[1]["Event"]):
                     try:
-                        (item[1]["Year"], item[1]["Season"], item[1]["City"])
+                        register_game_event(item[1]["Year"], item[1]["Season"], item[1]["City"], item[1]["Event"])
                     except:
                         register_message(item[1]["Event"], message_except, request)
 
@@ -207,8 +212,96 @@ def game_event_not_exist(year, season, city, event):
         event=get_event_by_name(event)
     ).first() == None
 
+def get_game_event_not_exist(year, season, city, event):
+    return GameEvents.objects.get(
+        game=get_game_by_data(year, season, city),
+        event=get_event_by_name(event)
+    )
+
 def register_game_event(year, season, city, event):
     GameEvents.objects.create(
         game=get_game_by_data(year, season, city),
         event=get_event_by_name(event)
+    )
+
+def save_athlete(athlete, request):
+    df = athlete.drop_duplicates("ID", keep='first')
+    df[['Name', 'Sex']].fillna(value=" ", inplace=True)
+    df['Height'].fillna(value=df['Height'].mean(), inplace=True)
+    df['Weight'].fillna(value=df['Weight'].mean(), inplace=True)
+
+    message_except = "Erro ao cadastrar o atleta "
+    for item in df.iterrows():
+        if not region_not_exist(item[1]["NOC"]):
+            if not sport_not_exist(item[1]["Sport"]):
+                if athlete_not_exist(item[1]["Name"],item[1]["Sex"],
+                                     item[1]["Height"],item[1]["Weight"],
+                                     item[1]["NOC"], item[1]["Sport"]):
+                    try:
+                        register_atlete(item[1]["Name"],item[1]["Sex"],item[1]["Height"],
+                                        item[1]["Weight"],item[1]["NOC"], item[1]["Sport"])
+                    except:
+                        register_message(item[1]["ID"]+" "+item[1]["Name"], message_except, request)
+
+def athlete_not_exist(name, sex, height, weight, noc, sport):
+    return Athlete.objects.filter(
+        name= name,
+        sex=sex,
+        height= height,
+        weight= weight,
+        team= get_region_by_noc(noc),
+        sport= get_sport_by_name(sport)
+    ).first() == None
+
+def get_athlete_not_exist(name, sex, height, weight, noc, sport):
+    return Athlete.objects.get(
+        name= name,
+        sex=sex,
+        height= height,
+        weight= weight,
+        team= get_region_by_noc(noc),
+        sport= get_sport_by_name(sport)
+    )
+
+def register_atlete(name, sex, height, weight, noc, sport):
+    Athlete.objects.create(
+        name= name,
+        sex=sex,
+        height= height,
+        weight= weight,
+        team= get_region_by_noc(noc),
+        sport= get_sport_by_name(sport)
+    )
+
+def save_event_participants(athlete, request):
+    athlete[['Name', 'Sex', 'Medal']].fillna(value="", inplace=True)
+    athlete['Height'].fillna(value=athlete['Height'].mean(), inplace=True)
+    athlete['Weight'].fillna(value=athlete['Weight'].mean(), inplace=True)
+    athlete.sort_values("Name", inplace=True)
+
+    message_except = "Erro ao cadastrar o atleta no evento"
+    for item in athlete.iterrows():
+        if not region_not_exist(item[1]["NOC"]):
+            if not sport_not_exist(item[1]["Sport"]):
+                if not game_event_not_exist(item[1]["Year"], item[1]["Season"], item[1]["City"], item[1]["Event"]):
+                    if not athlete_not_exist(item[1]["Name"], item[1]["Sex"], item[1]["Height"], item[1]["Weight"],  item[1]["NOC"], item[1]["Sport"]):
+                        if event_participant_not_exist(item[1]["Name"], item[1]["Sex"], item[1]["Height"], item[1]["Weight"],  item[1]["NOC"], item[1]["Sport"], item[1]["Year"], item[1]["Season"], item[1]["City"], item[1]["Event"]):
+                            try:
+                                register_event_participants(item[1]["Name"], item[1]["Sex"], item[1]["Height"], item[1]["Weight"],
+                                                item[1]["NOC"], item[1]["Sport"], item[1]["Year"], item[1]["Season"],
+                                                item[1]["City"], item[1]["Event"])
+                            except:
+                                register_message(item[1]["Name"]+" "+item[1]["Event"], message_except, request)
+
+
+def event_participant_not_exist(name, sex, height, weight,  noc, sport, year, season, city, event):
+    return EventParticipant.objects.filter(
+        game_event=get_game_event_not_exist(year, season, city, event),
+        athlete=get_athlete_not_exist(name, sex, height, weight, noc, sport)
+    ).first() == None
+
+def register_event_participants(name, sex, height, weight, noc, sport, year, season, city, event):
+    EventParticipant.objects.create(
+        game_event=get_game_event_not_exist(year, season, city, event),
+        athlete=get_athlete_not_exist(name, sex, height, weight, noc, sport)
     )
