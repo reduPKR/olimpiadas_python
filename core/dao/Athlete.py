99cf2a84414ce49fd4b73f24033c43117a403c5c
redupkr <reduardo1994@hotmail.com>
2020-10-28 12:12:37 -0300

    Filtro por esportes

--- core/dao/Athlete.py
+++ core/dao/Athlete.py
@@ -6,18 +6,25 @@ def list_all():
     return models.Athlete.objects.all().order_by('name')
 
 def filter_by_team_id(team_id):
-    return  list(models.Athlete.objects.filter(team=team_id))
+    return list(models.Athlete.objects.filter(team=team_id))
+
+def filter_by_sport_id(sport_id):
+    return list(models.Athlete.objects.filter(sport=sport_id))
 
 def filter(name, age, height, weight, team_id, game_id, event_id, sport_id, city_id, gold, silver, bronze):
     athletes = []
     athletes_medal = []
     athletes_team = []
+    athletes_sport = []
 
     if team_id is not "0":
         athletes_team = filter_by_team_id(team_id)
+
+    if sport_id is not "0":
+        athletes_sport = filter_by_sport_id(sport_id)
 
     if gold is not None or silver is not None or bronze is not None:
         medals = Medal.filter(gold, silver, bronze)
         athletes_medal = EventParticipants.filter_get_athlete_medals(medals)
 
-    return athletes_team
+    return athletes_sport
